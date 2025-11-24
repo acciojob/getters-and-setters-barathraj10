08ae872909455492320589efc2db37e49820b54c
Barath Raj K <157716432+barathraj10@users.noreply.github.com>
2025-11-24 22:34:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
-//complete this code
 class Person {
-	
-	constructor(name, age){
-		this.name=name;
-		this.age=age;
+	constructor(name, age) {
+		this._name = name;
+		this._age = age;
 	}
-	get name(){
-		return this.name;
+
+	// Getter for name
+	get name() {
+		return this._name;
 	}
-	set name(age){
-		this.age=age;
+
+	// Setter for name (optional but allowed)
+	set name(name) {
+		this._name = name;
+	}
+
+	// Getter for age
+	get age() {
+		return this._age;
+	}
+
+	// Setter for age
+	set age(age) {
+		this._age = age;
 	}
 }
 
 class Student extends Person {
-	study(){
+	study() {
 		console.log(`${this.name} is studying`);
 	}
 }
 
 class Teacher extends Person {
-	teach(){
-		console.log(this.name+" is teaching");
+	teach() {
+		console.log(`${this.name} is teaching`);
 	}
 }
 
